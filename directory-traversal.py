5b355caa079f1dea47a7810b7ee8dfec883448f1
Subhajit <43522742+Subho-n5@users.noreply.github.com>
2024-10-09 15:24:26 +0530

    Update directory-traversal.py

--- directory-traversal.py
+++ directory-traversal.py
@@ -1,10 +1,11 @@
+#Outer loop is for outer folder structure
 for i in {01..31}
 do
 	cd $i
-	for j in {00..23}
+	for j in {00..23} #THis inner loop is for the folder within the outer folder
 	do
 		cd $j
-		cat aws-waf-logs* | egrep -o '"clientIp": "[^"]*", "country": "[^"]*"' | grep -v '"country": "IN"'
+		cat aws-waf-logs* | egrep -o '"clientIp": "[^"]*", "country": "[^"]*"' | grep -v '"country": "IN"' #View specific pattern named file within each folder. Also, grep some specific data
 		cd ../
 	done
 	cd ../
